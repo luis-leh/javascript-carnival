caa6d93d4ad6a6e06bd5d7fb2b25e88b29b6fad6
Luis-Leh <luis_leh@yahoo.com>
2022-05-15 16:39:27 +0800

    tyring to add onclick event

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -5,31 +5,31 @@
 console.log('Inflate The Unicorn!')
 
 // Write a function that gets called every time you click on a unicorn
-function inflateUnicorn(unihorn) {
-  switch (unihorn) {
-    case document.getElementById('first-unicorn').src ==
-      './images/unicorn-0.png':
-      return (
-        document.getElementById('first-unicorn').src == './images/unicorn-1.png'
-      )
-      break
-    case document.getElementById('first-unicorn').src ==
-      './images/unicorn-1.png':
-      return (
-        document.getElementById('first-unicorn').src == './images/unicorn-2.png'
-      )
-      break
-    case document.getElementById('first-unicorn').src ==
-      './images/unicorn-2.png':
-      return (
-        document.getElementById('first-unicorn').src == './images/unicorn-3.png'
-      )
-      break
-    case document.getElementById('first-unicorn').src ==
-      './images/unicorn-3.png':
-      return 'THANK YOU VERY MUCH!'
-      break
-    default:
-      return 'PLEASE HELP US!'
-  }
-}
+// function inflateUnicorn() {
+//   switch () {
+//     case document.getElementById('first-unicorn').src ==
+//       './images/unicorn-0.png':
+//       return (
+//         document.getElementById('first-unicorn').src == './images/unicorn-1.png'
+//       )
+//       break
+//     case document.getElementById('first-unicorn').src ==
+//       './images/unicorn-1.png':
+//       return (
+//         document.getElementById('first-unicorn').src == './images/unicorn-2.png'
+//       )
+//       break
+//     case document.getElementById('first-unicorn').src ==
+//       './images/unicorn-2.png':
+//       return (
+//         document.getElementById('first-unicorn').src == './images/unicorn-3.png'
+//       )
+//       break
+//     case document.getElementById('first-unicorn').src ==
+//       './images/unicorn-3.png':
+//       return 'THANK YOU VERY MUCH!'
+//       break
+//     default:
+//       return 'PLEASE HELP US!'
+//   }
+// }
