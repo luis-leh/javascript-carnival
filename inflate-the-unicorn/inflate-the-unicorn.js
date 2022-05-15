5d4eb5dee657cf2aa03b954280bb67b22e199980
Luis-Leh <luis_leh@yahoo.com>
2022-05-15 13:49:24 +0800

    adding case operator

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -3,3 +3,33 @@
 // -    -   -   -   -  //
 
 console.log('Inflate The Unicorn!')
+
+// Write a function that gets called every time you click on a unicorn
+function inflateUnicorn(unihorn) {
+  switch (unihorn) {
+    case document.getElementById('first-unicorn').src ==
+      './images/unicorn-0.png':
+      return (
+        document.getElementById('first-unicorn').src == './images/unicorn-1.png'
+      )
+      break
+    case document.getElementById('first-unicorn').src ==
+      './images/unicorn-1.png':
+      return (
+        document.getElementById('first-unicorn').src == './images/unicorn-2.png'
+      )
+      break
+    case document.getElementById('first-unicorn').src ==
+      './images/unicorn-2.png':
+      return (
+        document.getElementById('first-unicorn').src == './images/unicorn-3.png'
+      )
+      break
+    case document.getElementById('first-unicorn').src ==
+      './images/unicorn-3.png':
+      return 'THANK YOU VERY MUCH!'
+      break
+    default:
+      return 'PLEASE HELP US!'
+  }
+}
