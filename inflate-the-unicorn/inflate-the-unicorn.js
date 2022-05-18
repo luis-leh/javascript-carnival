b688fcdae275076b5b15ddc3f4ed47d50f8e5c46
Luis-Leh <luis_leh@yahoo.com>
2022-05-19 05:48:03 +0800

    starting new

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -6,21 +6,21 @@ console.log('Inflate The Unicorn!')
 
 // Write a function that gets called every time you click on a unicorn
 
-let myImage = document.querySelector('inflate-an-image')
+// let myImage = document.querySelector('inflate-an-image')
 
-myImage.onclick = function () {
-  let mySrc = myImage.getAttribute('src')
-  if (mySrc === './images/unicorn-0.png') {
-    myImage.setAttribute('src', './images/unicorn-1.png')
-  }
-  else if (mySrc === './images/unicorn-1.png') {
-    myImage.setAttribute('src', './images/unicorn-2.png')
-  }
-  else (mySrc === './images/unicorn-2.png') {
-    myImage.setAttribute('src', './images/unicorn-3.png')
-  }
-  break
-}
+// myImage.onclick = function () {
+//   let mySrc = myImage.getAttribute('src')
+//   if (mySrc === './images/unicorn-0.png') {
+//     myImage.setAttribute('src', './images/unicorn-1.png')
+//   }
+//   else if (mySrc === './images/unicorn-1.png') {
+//     myImage.setAttribute('src', './images/unicorn-2.png')
+//   }
+//   else (mySrc === './images/unicorn-2.png') {
+//     myImage.setAttribute('src', './images/unicorn-3.png')
+//   }
+//   break
+// }
 
 // function inflateUnicorn() {
 //   switch () {
