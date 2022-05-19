04e37ee575ece87c196c7137add134a7a1782927
Luis-Leh <luis_leh@yahoo.com>
2022-05-19 21:42:54 +0800

    changing head clothing

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -8,3 +8,43 @@ console.log('Dress The Clown!')
 
 // arrow keys left & right - clothing items
 // arrow keys up & down - clown parts
+
+document.onkeydown = checkKey
+
+// EVENT IS THE SAME WITH ONCLICK
+
+function checkKey(e) {
+  //  e = e || window.event (TO CATER FOR WINDOWS INTERNET EXPLORER)
+
+  console.log(e)
+
+  if (e.keyCode == '38') {
+    // up arrow
+  } else if (e.keyCode == '40') {
+    // down arrow
+  } else if (e.keyCode == '37') {
+    changeClothes(-1)
+    // left arrow
+  } else if (e.keyCode == '39') {
+    changeClothes(1)
+    // right arrow
+  }
+}
+
+function changeClothes() {}
+
+let headIndex = 0
+
+let head = document.getElementById('head')
+let body = document.getElementById('body')
+let feet = document.getElementById('feet')
+
+function changeClothes(change) {
+  headIndex += change
+
+  if (headIndex < 0) headIndex = 5
+
+  if (headIndex > 5) headIndex = 0
+
+  head.src = './images/head' + headIndex + '.png'
+}
