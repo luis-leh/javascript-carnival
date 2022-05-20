76cf35f984552e820558db0f4359b6ba4ddbcbf0
Luis-Leh <luis_leh@yahoo.com>
2022-05-20 17:41:17 +0800

    adding cells variable & randomIndex variable

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -8,3 +8,9 @@ console.log('Whack-a-Mole!')
 // MOLE img APPEARS IN A RANDOM CELL
 // CLICK ON THE MOLE
 // MOLE RESPAWNS IN ANOTHER CELL
+
+// Google - "how to get elements by tag name?"
+let cells = document.getElementsByTagName('td')
+
+// Google - "get random int in range"
+let randomIndex = Math.floor(Math.random() * cells.length)
