9bf8e9720acfbce54d6cca0351ec8ce98b82fcff
Luis-Leh <luis_leh@yahoo.com>
2022-05-20 17:23:58 +0800

    adding initial steps what to do

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -1,5 +1,10 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Whack-a-Mole!")
+console.log('Whack-a-Mole!')
+
+// GET A RANDOM CELL
+// MOLE img APPEARS IN A RANDOM CELL
+// CLICK ON THE MOLE
+// MOLE RESPAWNS IN ANOTHER CELL
